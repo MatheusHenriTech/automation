c0bbea51f8ed21ceb02e532a30b72b5d850bab6e
Matheus Henrique da Silva <5331588@alunouninter.com>
2026-05-21 21:45:12 -0300

    banco de dados

--- main.py
+++ main.py
@@ -2,27 +2,79 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from datetime import datetime, timedelta
+from sqlalchemy import create_engine, Column, String, Integer, DateTime
+from sqlalchemy.orm import sessionmaker, declarative_base
+from datetime import datetime, timedelta
+
+engine = create_engine("sqlite:///database.db")
+
+Session = sessionmaker(engine)
+Base = declarative_base()
 
 permissions = discord.Intents.default().all()
 bot = commands.Bot(command_prefix='/', intents=permissions)
 
 users = {}
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String)
+    username = Column(String)
+    coins = Column(Integer, default=0)
+    daily = Column(DateTime, default=None)
+    
+Base.metadata.create_all(engine)
+
 @bot.event
 async def on_ready():
     await bot.tree.sync()
     print('Synchronized')
 
+ 
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    
+    session = Session()
+
+    usuario = session.query(User).filter_by(
+        user_id = str(message.author.id)
+    ).first()
+
+    if not usuario:
+        usuario = User(
+            user_id = str(message.author.id),
+            username = message.author.name,
+            coins=0
+        )
+
+        session.add(usuario)
+
+    usuario.coins += 1
+    session.commit()
+
+
+    await bot.process_commands(message)
+
+
 @bot.tree.command()
 async def perfil(interaction: discord.Interaction):
-    user_id = interaction.user.id
+    
+    session = Session()
 
-    if user_id not in users:
-        users[user_id] = {
-            'coins':0
-    }
-        
-    coins = users[user_id]['coins']
+    usuario = session.query(User).filter_by(
+        user_id = str(interaction.user.id)
+    ).first()
+
+    if not usuario:
+        usuario = User(
+            user_id=interaction.user.id,
+            username=interaction.user.name,
+            coins=0
+        )
 
     embed = discord.Embed(
         title="title",
@@ -31,19 +83,19 @@ async def perfil(interaction: discord.Interaction):
 
     embed.add_field(
         name='Name:',
-        value=interaction.user.display_name,
+        value=usuario.username,
         inline=False
     )
 
     embed.add_field(
         name='ID:',
-        value=user_id,
+        value=usuario.id,
         inline=False
     )
 
     embed.add_field(
         name="Coins",
-        value=coins,
+        value=usuario.coins,
         inline=False
     )  
 
@@ -51,54 +103,68 @@ async def perfil(interaction: discord.Interaction):
         url=interaction.user.avatar.url
     )
 
-    await interaction.response.send_message(embed=embed, ephemeral=True)
- 
-@bot.event
-async def on_message(message):
-    user_id = message.author.id
-    if user_id not in users:
-        users[user_id] = {
-            'coins':0
-        }
+    session.add(usuario)
+    session.commit()
 
-    coins = users[user_id]['coins']
-    users[user_id]['coins']+=1
-    
-    await bot.process_commands(message)
+    await interaction.response.send_message(embed=embed, ephemeral=True)
+
 
 @bot.tree.command()
 async def daily(interaction: discord.Interaction):
+
+    session = Session()
+
     embed = discord.Embed(
         title='Daily awards',
         description='get your awards right now!',
         color=discord.Color.fuchsia()
     )
 
-    user_id = interaction.user.id
+    usuario = session.query(User).filter_by(
+        user_id = str(interaction.user.id)
+    ).first()
 
-    if user_id not in users:
-        users[user_id] = {
-            'coins':0,
-            'date':None
-        }
+    if not usuario:
+        usuario = User(
+            user_id=interaction.user.id,
+            username=interaction.user.name,
+            coins=0
+        )
 
-    last_daily = users[user_id].get('date')
-    now = datetime.now()
-    if last_daily is not None and now - last_daily < timedelta(hours=24):
-        embed.description="You've already got the daily coins bonus"
-        await interaction.response.send_message(embed=embed, ephemeral=True)
-        return
+    actually_daily = datetime.now()
+
+    if usuario.daily is not None:
+        if actually_daily - usuario.daily < timedelta(hours=24):
+            embed.description=f"You alreay have got the daily awards, you'll need to wait pass 24 hours since you had recue the daily awards "
+            return await interaction.response.send_message(embed=embed, ephemeral=True)
     
-    users[user_id]['coins'] += 100
-    users[user_id]['date'] = now
+    usuario.coins += 100
+    usuario.daily = actually_daily
+        
+    session.add(usuario)
+    session.commit()
 
-    coins = users[user_id]['coins']
-
-    embed.description=f"You just won 100 coins\nNow you have {coins} coins"
+    embed.description=f"You just won 100 coins\nNow you have {usuario.coins} coins"
     await interaction.response.send_message(embed=embed, ephemeral=True)
+
+
 
 @bot.tree.command()
 async def donate_coins(interaction: discord.Interaction, receiver: discord.Member, coin:int):
+
+    session = Session()
+
+    usuario = session.query(User).filter_by(
+        user_id = str(interaction.user.id)
+    ).first()
+
+    if not usuario:
+        usuario = User(
+            user_id=interaction.user.id,
+            username=interaction.user.name,
+            coins=0    
+        )
+        
     
     embed = discord.Embed(
         title='Donate Coins',
@@ -106,55 +172,64 @@ async def donate_coins(interaction: discord.Interaction, receiver: discord.Membe
         color=discord.Color.fuchsia()
     )
 
-    user_id = interaction.user.id
-    if user_id not in users:
-        users[user_id] = {
-            'coins':0
-        }
-    coins = users[user_id]['coins']
-    if coin > coins or coin <= 0:
+    coins_user = usuario.coins
+    if coin > coins_user or coin <= 0:
         embed.description = f"Sorry, You ain't donate this quantity of coins :/"
-        await interaction.response.send_message(embed=embed, ephemeral=True)
-        return
-    receiver_id = receiver.id
-    if receiver.id not in users:
-        users[receiver.id] = {
-            'coins':0
-        }
-    users[user_id]['coins'] -= coin
-    users[receiver_id]['coins'] += coin
+        return await interaction.response.send_message(embed=embed, ephemeral=True)
+    
+    receiver_user = session.query(User).filter_by(
+        user_id = str(receiver.id)
+    ).first()
 
-    embed.description = f"You've just donated {coins} coins to {receiver}"
+    if not receiver_user:
+        embed.description = f"This user does not exist :/"
+        return await interaction.response.send_message(embed=embed, ephemeral=True)
+    
+    usuario.coins -= coin
+    receiver_user.coins += coin
+
+    session.add(usuario)
+    session.commit()
+
+    embed.description = f"You've just donated {coin} coins to {receiver_user.username}"
     await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
 @bot.tree.command()
 async def ranking(interaction: discord.Interaction):
 
+    session = Session()
+
+    usuario = session.query(User).filter_by(
+        user_id = str(interaction.user.id)
+    ).first()
+
+    if not usuario:
+        usuario = User(
+            user_id=interaction.user.id,
+            username=interaction.user.name,
+            coins=0      
+        )
+        
     embed = discord.Embed(
         title='Ranking',
         description='Ranking of the most rich users',
         color=discord.Color.fuchsia()
     )
 
-    list_users = []
-    ranking_text = []
-    if users:
-        for p in users.items():
-            member = interaction.guild.get_member(p[0])
-            coins = p[1]['coins']
-            user = (member.display_name, coins)
-            list_users.append(user)
-            list_users.sort(key=lambda x: x[1], reverse=True)
-        for nome, coins in list_users:
-            txt = f'Name: {nome}\nCoins: {coins}'
-            ranking_text.append(txt)
-        final_text = '\n\n'.join(ranking_text)
-        embed.description = final_text
-        await interaction.response.send_message(embed=embed)
-    else:
-        embed.description='There no people here'
-        await interaction.response.send_message(embed=embed)
+    list_ranking = []
+    list_user = session.query(User).order_by(User.coins.desc()).all()
+    for user in list_user:
+        name_user= f'User: {user.username}'
+        coin_user = f'Coins: {user.coins}\n'
+        answer_ranking = f'User: {user.username}\nCoins: {user.coins}'
+        list_ranking.append(f'{answer_ranking}')
+    list_formated = '\n\n'.join(list_ranking)
+    session.add(usuario)
+    session.commit()
+
+    embed.description = list_formated
+    await interaction.response.send_message(embed=embed)
 
 
-bot.run("MTUwNDI0MjEzMDg5NjI5MzkzOQ.G6UnZd.WYHgXr4O3bstO6QtmtStnG0r3sAyJHI5Yrgb3Y")
+bot.run("MTUwNDI0MjEzMDg5NjI5MzkzOQ.GKJ9KW.7Jqs0KN8qhVSKdYjACHF-F35BsmNZIUehQmcso")
--- teste.py
+++
@@ -1,4 +0,0 @@
-lista = [('Matheus', 50), ('Matias', 10)]
-
-lista.sort(key=lambda x: x[1], reverse=True)
-print(lista)
